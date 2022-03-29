559ff2dc20b9362bb82770b24257954dd14c8018
costea <costeacionut@gmail.com>
2022-03-30 00:40:28 +0300

    Fix user request for navbar after authchange

--- PlatformaRecrutari.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ PlatformaRecrutari.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { UserInfo } from '../../shared/interfaces/user/userInfo';
 import { AuthenticationService } from '../../shared/services/authentication.service';
 
@@ -8,7 +8,7 @@ import { AuthenticationService } from '../../shared/services/authentication.serv
   styleUrls: ['./nav-menu.component.css']
 })
 
-export class NavMenuComponent implements OnInit {
+export class NavMenuComponent implements OnInit, OnChanges {
 
   public isUserAuthenticated: boolean;
   user: UserInfo
@@ -16,20 +16,24 @@ export class NavMenuComponent implements OnInit {
 
   constructor(private _authService: AuthenticationService) {
   }
+    ngOnChanges(changes: SimpleChanges): void {
+        throw new Error('Method not implemented.');
+    }
 
   async ngOnInit() {
     await this._authService._navBarNotification.authChanged
       .subscribe(res => {
         this.isUserAuthenticated = res;
+
+        if (this.isUserAuthenticated) {
+          this._authService.getCurrentUser().subscribe(user => {
+            this.user = user
+          }, err => console.error(err))
+        }
+
       })
 
     await this._authService._navBarNotification.sendLoginStateNotification(this._authService.isUserAuthenticated())
-
-    if (this.isUserAuthenticated) {
-      await this._authService.getCurrentUser().subscribe(user => {
-        this.user = user
-      }, err => console.error(err))
-    }
   }
 
   isUserPM() {
