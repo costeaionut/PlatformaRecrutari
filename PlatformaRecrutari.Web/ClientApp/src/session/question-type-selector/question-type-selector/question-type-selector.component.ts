4661e1f5419e1376cba8356c34aee6a08dea0558
costea <costeacionut@gmail.com>
2022-05-29 21:23:47 +0300

    Create question-type-selector and add i/o props

---
+++ PlatformaRecrutari.Web/ClientApp/src/session/question-type-selector/question-type-selector/question-type-selector.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+
+@Component({
+  selector: "app-question-type-selector",
+  templateUrl: "./question-type-selector.component.html",
+  styleUrls: ["./question-type-selector.component.css"],
+})
+export class QuestionTypeSelectorComponent implements OnInit {
+  @Input() questionType: String;
+  @Output() updateType = new EventEmitter<String>();
+
+  constructor() {}
+
+  updateQuestionType() {
+    this.updateType.emit(this.questionType);
+  }
+
+  ngOnInit() {}
+}
