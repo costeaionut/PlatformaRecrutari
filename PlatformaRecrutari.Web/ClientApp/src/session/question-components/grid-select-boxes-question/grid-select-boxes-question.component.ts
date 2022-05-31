1f3dc67609395490d677d3806f37a6ba0ee040d5
costea <costeacionut@gmail.com>
2022-05-31 18:32:37 +0300

    Fix grud type, changed to grid

---
+++ PlatformaRecrutari.Web/ClientApp/src/session/question-components/grid-select-boxes-question/grid-select-boxes-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-select-boxes-question',
+  templateUrl: './grid-select-boxes-question.component.html',
+  styleUrls: ['./grid-select-boxes-question.component.css']
+})
+export class GridSelectBoxesQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
