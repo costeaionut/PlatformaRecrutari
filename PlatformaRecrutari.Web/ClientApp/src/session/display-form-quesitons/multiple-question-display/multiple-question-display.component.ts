851e3b343a8a3ee63deee42c7d39de0942212d32
costea <costeacionut@gmail.com>
2022-06-19 00:46:00 +0300

    Add communication for multiple-question and selbox

--- PlatformaRecrutari.Web/ClientApp/src/session/display-form-quesitons/multiple-question-display/multiple-question-display.component.ts
+++ PlatformaRecrutari.Web/ClientApp/src/session/display-form-quesitons/multiple-question-display/multiple-question-display.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { MultipleQuestion } from "src/shared/classes/questions/multiple-question";
+import { AnswerPosition } from "src/shared/interfaces/session/answer-position";
 
 @Component({
   selector: "app-multiple-question-display",
@@ -10,12 +11,25 @@ export class MultipleQuestionDisplayComponent implements OnInit {
   @Input() question: MultipleQuestion;
   @Input() position: number;
   @Input() canAnswer: boolean;
-
+  @Input() answer: string;
+  @Output() changeAnswer: EventEmitter<AnswerPosition> =
+    new EventEmitter<AnswerPosition>();
   options: string[];
   selectedOption: string;
   constructor() {}
 
   ngOnInit() {
     this.options = this.question.getOptions();
+    if (this.answer != "" && this.options.includes(this.answer))
+      this.selectedOption = this.answer;
+  }
+
+  updateAnswer() {
+    let newAnswer: AnswerPosition = {
+      answer: this.selectedOption,
+      position: this.position,
+    };
+    console.log(newAnswer);
+    this.changeAnswer.emit(newAnswer);
   }
 }
--- PlatformaRecrutari.Web/ClientApp/src/session/display-form-quesitons/select-box-question-display/select-box-question-display.component.ts
+++ PlatformaRecrutari.Web/ClientApp/src/session/display-form-quesitons/select-box-question-display/select-box-question-display.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { SelectBoxesQuestion } from "src/shared/classes/questions/select-boxes-question";
+import { AnswerPosition } from "src/shared/interfaces/session/answer-position";
 
 @Component({
   selector: "app-select-box-question-display",
@@ -10,6 +11,10 @@ export class SelectBoxQuestionDisplayComponent implements OnInit {
   @Input() question: SelectBoxesQuestion;
   @Input() position: number;
   @Input() canAnswer: number;
+  @Input() answer: string;
+
+  @Output() changeAnswer: EventEmitter<AnswerPosition> =
+    new EventEmitter<AnswerPosition>();
 
   checkboxes = new Map<string, boolean>();
 
@@ -19,5 +24,27 @@ export class SelectBoxQuestionDisplayComponent implements OnInit {
     this.question.getOptions().forEach((option) => {
       this.checkboxes[option] = false;
     });
+    if (this.answer != "") {
+      let splitAnswersInput: Array<string> = this.answer.split(";;");
+      splitAnswersInput.forEach((option) => {
+        if (this.question.getOptions().includes(option))
+          this.checkboxes[option] = true;
+      });
+    }
+  }
+  updateAnswer() {
+    this.answer = "";
+    this.question.getOptions().forEach((option) => {
+      if (this.answer == "") {
+        if (this.checkboxes[option]) this.answer += `${option}`;
+      } else if (this.checkboxes[option]) this.answer += `;;${option}`;
+    });
+
+    let newAnswer: AnswerPosition = {
+      answer: this.answer,
+      position: this.position,
+    };
+
+    this.changeAnswer.emit(newAnswer);
   }
 }
