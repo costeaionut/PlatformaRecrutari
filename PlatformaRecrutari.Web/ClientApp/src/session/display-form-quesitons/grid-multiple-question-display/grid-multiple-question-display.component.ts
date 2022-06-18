15238074e5c3acb48c46ecd12ba33f424feca251
costea <costeacionut@gmail.com>
2022-06-18 18:57:40 +0300

    Add grid multiple question and validations

--- PlatformaRecrutari.Web/ClientApp/src/session/display-form-quesitons/grid-multiple-question-display/grid-multiple-question-display.component.ts
+++ PlatformaRecrutari.Web/ClientApp/src/session/display-form-quesitons/grid-multiple-question-display/grid-multiple-question-display.component.ts
@@ -11,7 +11,54 @@ export class GridMultipleQuestionDisplayComponent implements OnInit {
   @Input() position: number;
   @Input() canAnswer: boolean;
 
+  error: string;
+  touched: boolean;
+  hasError: boolean;
+  answerGrid: Array<number>;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.answerGrid = Array(this.question.getRows().length).fill(-1);
+    this.touched = false;
+    this.hasError = false;
+    this.error = "";
+  }
+
+  findDuplicates = (arr) =>
+    arr.filter((item, index) => arr.indexOf(item) !== index);
+
+  checkForErrors(): boolean {
+    if (this.touched) {
+      if (this.question.getOneAnswearPerColumn()) {
+        let duplicatedItems: number[] = this.findDuplicates(this.answerGrid);
+        for (let i = 0; i < duplicatedItems.length; i++) {
+          if (duplicatedItems[i] != -1) {
+            this.hasError = true;
+            this.error = "Please choose only one answer per column!";
+            return true;
+          }
+        }
+      }
+      if (this.question.getRequired()) {
+        for (let i = 0; i < this.answerGrid.length; i++) {
+          if (this.answerGrid[i] == -1) {
+            this.hasError = true;
+            this.error = "Please choose at leas one answer per row!";
+            return true;
+          }
+        }
+      }
+    }
+
+    this.hasError = false;
+    this.error = "";
+    return false;
+  }
+
+  selectCheckbox(row: number, col: number) {
+    this.touched = true;
+    this.answerGrid[row] = col;
+    this.checkForErrors();
+  }
 }
