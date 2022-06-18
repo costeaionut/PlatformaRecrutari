4b98bcb25584d7d3731a645c70fda7a66a7445ca
costea <costeacionut@gmail.com>
2022-06-18 17:13:37 +0300

    Add gird select box validation

--- PlatformaRecrutari.Web/ClientApp/src/session/display-form-quesitons/grid-select-box-question-display/grid-select-box-question-display.component.ts
+++ PlatformaRecrutari.Web/ClientApp/src/session/display-form-quesitons/grid-select-box-question-display/grid-select-box-question-display.component.ts
@@ -11,7 +11,69 @@ export class GridSelectBoxQuestionDisplayComponent implements OnInit {
   @Input() position: number;
   @Input() canAnswer: boolean;
 
+  answerGrid: Array<Array<boolean>>;
+  hasError: boolean;
+  error: string;
+  touched: boolean;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.answerGrid = new Array<Array<boolean>>();
+    this.hasError = false;
+    this.touched = false;
+    this.error = "";
+
+    this.question.getRows().forEach((row) => {
+      let lenght = this.question.getColumns().length;
+      this.answerGrid.push(Array(lenght).fill(false));
+    });
+  }
+
+  checkForErrors(): boolean {
+    if (this.touched) {
+      if (this.question.getOneAnswearPerColumn()) {
+        for (let j = 0; j < this.answerGrid[0].length; j++) {
+          let hasMultipleOptionsSelected: boolean = false;
+          let numberOfSelected: number = 0;
+          for (let i = 0; i < this.answerGrid.length; i++) {
+            if (this.answerGrid[i][j]) numberOfSelected++;
+            if (numberOfSelected > 1) hasMultipleOptionsSelected = true;
+          }
+          if (hasMultipleOptionsSelected) {
+            this.hasError = true;
+            this.error = "Please select only one answer per column!";
+            return true;
+          }
+        }
+      }
+
+      if (this.question.getRequired()) {
+        for (let i = 0; i < this.answerGrid.length; i++) {
+          let hasSelectedOption: boolean = false;
+          for (let j = 0; j < this.answerGrid[i].length; j++) {
+            if (this.answerGrid[i][j] == true) hasSelectedOption = true;
+          }
+          if (!hasSelectedOption) {
+            this.hasError = true;
+            this.error = "Please select at least one answer per row!";
+            return true;
+          }
+        }
+      }
+    }
+
+    this.hasError = false;
+    this.error = "";
+    return false;
+  }
+
+  constructAnswer() {}
+
+  selectOption(rowIndex: number, colIndex: number) {
+    this.touched = true;
+    this.answerGrid[rowIndex][colIndex] = !this.answerGrid[rowIndex][colIndex];
+    this.checkForErrors();
+    this.constructAnswer();
+  }
 }
