4e9b3e5477ea099c506bdd69622a7e739e118949
costea <costeacionut@gmail.com>
2022-06-27 21:41:29 +0300

    Create table

---
+++ PlatformaRecrutari.Data/Migrations/20220627183630_AddInterviewScheduleTable.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlatformaRecrutari.Data.Migrations
+{
+    public partial class AddInterviewScheduleTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InterviewSchedule",
+                columns: table => new
+                {
+                    ParticipantId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    InterviewId = table.Column<int>(type: "int", nullable: false),
+                    VolunteerId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InterviewSchedule", x => new { x.ParticipantId, x.InterviewId });
+                    table.ForeignKey(
+                        name: "FK_InterviewSchedule_Interviews_InterviewId",
+                        column: x => x.InterviewId,
+                        principalTable: "Interviews",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_InterviewSchedule_Users_VolunteerId",
+                        column: x => x.VolunteerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InterviewSchedule_InterviewId",
+                table: "InterviewSchedule",
+                column: "InterviewId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InterviewSchedule_VolunteerId",
+                table: "InterviewSchedule",
+                column: "VolunteerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InterviewSchedule");
+        }
+    }
+}
